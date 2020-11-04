cdb966f4907fd696076bba1a7bccff8dfdef3518
Holt-e <56269370+Holt-e@users.noreply.github.com>
2020-11-04 14:40:03 +0100

    Lets GO!!!

---
+++ src/main/java/BrukerEAO.java
@@ -0,0 +1,29 @@
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Stateless
+public class BrukerEAO {
+    @PersistenceContext(name = "DeltakerPU")
+    private EntityManager em;
+
+    public void leggTilbruker(Bruker s) {
+        em.persist(s);
+    }
+
+    public Bruker hentBruker(String mobil) {
+        return em.find(Bruker.class, mobil);
+    }
+
+    public List<Bruker> hentBrukere() {
+        //TypedQuery<Bruker> query = em.createQuery("SELECT c FROM bruker c", Bruker.class);
+        //String queryString = "select e from Bruker e";
+        //TypedQuery<Bruker> query = em.createQuery(queryString, Bruker.class);
+        //List<Bruker> results = query.getResultList();
+        //List al = em.createQuery("SELECT fornavn FROM dat108oblig3.bruker").getResultList();
+        //return results;
+
+        return em.createQuery("SELECT s FROM Bruker s").getResultList();
+    }
+}
---
+++ src/main/java/Paamelding.java
@@ -0,0 +1,53 @@
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+
+@WebServlet(name = "Paamelding", urlPatterns = "/paamelding")
+public class Paamelding extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    @EJB
+    private BrukerEAO brukerEAO;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        no.hvl.dat108.paamelingsObjekt skjema = new no.hvl.dat108.paamelingsObjekt(request);
+
+        if(skjema.isAllInputGyldig()){
+            Bruker bruker = new Bruker(request);
+
+            brukerEAO.leggTilbruker(bruker);
+            HttpSession sesjon = request.getSession(false);
+            if(sesjon != null) {
+                sesjon.invalidate();
+            }
+            sesjon = request.getSession(true);
+            sesjon.setMaxInactiveInterval(100000);
+
+            sesjon.setAttribute("bruker", bruker);
+
+            sesjon.setAttribute("fornavn", bruker.getFornavn());
+            sesjon.setAttribute("etternavn", bruker.getEtternavn());
+            sesjon.setAttribute("mobil", bruker.getMobil());
+            sesjon.setAttribute("kjonn", bruker.getKjonn());
+
+            response.sendRedirect("/paameldingsBekreftelse");
+        }else{
+            skjema.settOppFeilmeldinger();
+            request.getSession().setAttribute("skjema", skjema);
+            response.sendRedirect("/paamelding");
+        }
+
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("WEB-INF/Paamelding.jsp").forward(request, response);
+    }
+}
